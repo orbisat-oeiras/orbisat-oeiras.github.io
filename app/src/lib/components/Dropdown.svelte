988bfca6880e4b4f920e5c4de7d0d8e7a07a6224
Leviathan <levi.mcgomes@gmail.com>
2024-01-13 14:42:20 +0000

    Disabled annoying warnings.

--- app/src/lib/components/Dropdown.svelte
+++ app/src/lib/components/Dropdown.svelte
@@ -17,6 +17,8 @@
 		/></button
 	>
 	{#if isExpanded}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
 		<div
 			on:click={() => {
 				isExpanded = false;
