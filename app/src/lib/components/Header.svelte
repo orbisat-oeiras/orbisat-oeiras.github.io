59a9a2fd54dd3fc994a618ad07fc10fdd1f6bf0c
Leviathan <levi.mcgomes@gmail.com>
2024-01-17 18:40:23 +0000

    Revert "Future-proofed the links in the header."
    This reverts commit a963e0e415d0309d09f20ba145346d0b0cf4d5bd.

--- app/src/lib/components/Header.svelte
+++ app/src/lib/components/Header.svelte
@@ -25,13 +25,13 @@
 	<div class="flex gap-6 content-center [&>*]:h-min">
 		<Dropdown label="Sobre">
 			<!--Removed the / behind the # because it was causing issues-->
-			<a class="text-cansat-cream dark:text-cansat-black" href="#cansat">CanSat</a>
-			<a class="text-cansat-cream dark:text-cansat-black" href="/#project">O&nbsp;Projeto</a>
-			<a class="text-cansat-cream dark:text-cansat-black" href="/#team">A&nbsp;Equipa</a>
+			<a class="text-cansat-cream dark:text-cansat-black" href="/24#cansat">CanSat</a>
+			<a class="text-cansat-cream dark:text-cansat-black" href="/24#project">O&nbsp;Projeto</a>
+			<a class="text-cansat-cream dark:text-cansat-black" href="/24#team">A&nbsp;Equipa</a>
 		</Dropdown>
 		<!--<a class="text-grey-900" href="/24/about">Sobre</a>-->
-		<a class="text-cansat-cream" href="/#blog">Blog</a>
-		<a class="text-cansat-cream" href="/#sponsor">Apoios</a>
+		<a class="text-cansat-cream" href="/24#blog">Blog</a>
+		<a class="text-cansat-cream" href="/24#sponsor">Apoios</a>
 		<ThemeToggle />
 	</div>
 </nav>
